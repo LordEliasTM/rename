7958b8e5fe0a9e1d552c7177dd2f632007444c8c
LordElias <kerlinelias@gmail.com>
2023-11-30 22:36:20 +0100

    improve build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm -rf ./build/*
 env GOOS=windows GOARCH=amd64 go build -ldflags "-s" -o build/rename-win64.exe
 env GOOS=linux GOARCH=amd64 go build -ldflags "-s" -o build/rename-linux64
 env GOOS=darwin GOARCH=amd64 go build -ldflags "-s" -o build/rename-darwin64
