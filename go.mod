124ed37531be0c2d01443dab6fb02b3452d1bdc6
LordElias <kerlinelias@gmail.com>
2023-12-02 18:18:30 +0100

    rework colors

--- go.mod
+++ go.mod
@@ -5,12 +5,15 @@ go 1.21.4
 require (
 	github.com/dlclark/regexp2 v1.10.0
 	github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807
+	github.com/fatih/color v1.16.0
 	github.com/sergi/go-diff v1.3.1
 	github.com/spf13/cobra v1.8.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 )
--- strdiff/strdiff.go
+++ strdiff/strdiff.go
@@ -3,8 +3,12 @@ package strdiff
 import (
 	"bytes"
 
+	"github.com/fatih/color"
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
+
+var red = color.New(color.BgRed).Add(color.FgBlack)
+var green = color.New(color.BgGreen).Add(color.FgBlack)
 
 func CoolFunc(diffs []diffmatchpatch.Diff) string {
 	var buff1 bytes.Buffer
@@ -14,20 +18,17 @@ func CoolFunc(diffs []diffmatchpatch.Diff) string {
 		text := diff.Text
 
 		switch diff.Type {
-		case diffmatchpatch.DiffInsert:
-			_, _ = buff2.WriteString("\x1b[42m")
-			_, _ = buff2.WriteString(text)
-			_, _ = buff2.WriteString("\x1b[0m")
 		case diffmatchpatch.DiffDelete:
-			_, _ = buff1.WriteString("\x1b[41m")
-			_, _ = buff1.WriteString(text)
-			_, _ = buff1.WriteString("\x1b[0m")
+			_, _ = buff1.WriteString(red.Sprint(text))
+		case diffmatchpatch.DiffInsert:
+			_, _ = buff2.WriteString(green.Sprint(text))
 		case diffmatchpatch.DiffEqual:
 			_, _ = buff1.WriteString(text)
 			_, _ = buff2.WriteString(text)
 		}
 	}
 
+	// merge buffers and add "->" in between
 	buff1.WriteString(" -> ")
 	buff1.WriteString(buff2.String())
 	return buff1.String()
